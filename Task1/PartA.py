887db16c777b388f3a54841a36fd5e1215c090b7
Hassan-2305 <mohammedhassan2305@gmail.com>
2025-02-20 07:49:32 +0530

    Create PartA.py
    plotted user defined points in matplotlib using python

---
+++ Task1/PartA.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+x = []
+y = []
+
+points = int(input("enter how many points to plot "))
+
+for i in range(points):
+    x_coo = float(input(f"Enter the x-coordinate of point x(i + 1): "))
+    y_coo = float(input(f"Enter the y-coordinate of point y(i + 1): "))
+    x.append(x_coo)
+    y.append(y_coo)
+
+    plt.plot(x, y, marker ='o')
+
+    plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Plotting Points in Matplotlib')
+
+
+plt.show()
